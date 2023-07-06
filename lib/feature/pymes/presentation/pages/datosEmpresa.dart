f83ccbe2dc6dc2bb0840887d258d86e555dd2d83
Alonso Anselmo Gómez Sánchez <68868495+AAGS589@users.noreply.github.com>
2023-07-06 00:41:33 -0600

    aplicación de paginacion de stepper

--- lib/feature/pymes/presentation/pages/datosEmpresa.dart
+++ lib/feature/pymes/presentation/pages/datosEmpresa.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: file_names
+// ignore_for_file: file_names, non_constant_identifier_names
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -24,78 +24,54 @@ class DatosEmpresaPage extends StatelessWidget {
           onPressed: () {},
         ),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const SizedBox(height: 10),
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              children: List.generate(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(height: 10),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                children: List.generate(
                   4,
                   (index) => Stepper(
-                        currentIndex:
-                            Provider.of<StepperState>(context).currentIndex,
-                        index: index,
-                        isLast: index == 3,
-                        onTap: () {
-                          Provider.of<StepperState>(context, listen: false)
-                              .setIndex(index);
-                          _pageController.jumpToPage(index);
-                        },
-                      )),
+                    currentIndex:
+                        Provider.of<StepperState>(context).currentIndex,
+                    index: index,
+                    isLast: index == 3,
+                    onTap: () {
+                      Provider.of<StepperState>(context, listen: false)
+                          .setIndex(index);
+                      _pageController.jumpToPage(index);
+                    },
+                  ),
+                ),
+              ),
             ),
-          ),
-          Expanded(
+            Container(
+              height: MediaQuery.of(context).size.height *
+                  0.7, // Ajusta esto según tus necesidades
               child: PageView.builder(
-                  controller: _pageController,
-                  itemCount: 4,
-                  itemBuilder: (context, index) {
-                    switch (index) {
-                      case 0:
-                        return Container(
-                          height: 35.0,
-                          width: 35.0,
-                          decoration: const BoxDecoration(
-                            color: Colors.red,
-                          ),
-                        );
-                      case 1:
-                        return Container(
-                          height: 50.0,
-                          width: 50.0,
-                          decoration: const BoxDecoration(
-                            color: Color.fromARGB(255, 184, 75, 67),
-                          ),
-                        );
-                      case 2:
-                        return Container(
-                          height: 50.0,
-                          width: 50.0,
-                          decoration: const BoxDecoration(
-                            color: Color.fromARGB(255, 176, 193, 180),
-                          ),
-                        );
-                      case 3:
-                        return Container(
-                          height: 50.0,
-                          width: 50.0,
-                          decoration: const BoxDecoration(
-                            color: Color.fromARGB(255, 56, 15, 138),
-                          ),
-                        );
-                      case 4:
-                        return Container(
-                          height: 50.0,
-                          width: 50.0,
-                          decoration: const BoxDecoration(
-                            color: Color.fromARGB(255, 54, 244, 146),
-                          ),
-                        );
-                    }
-                    return null;
-                  })),
-        ],
+                controller: _pageController,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: 4,
+                itemBuilder: (context, index) {
+                  switch (index) {
+                    case 0:
+                      return Step1();
+                    case 1:
+                      return Step2();
+                    case 2:
+                      return Step3();
+                    case 3:
+                      return Step4();
+                  }
+                  return Container();
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -136,14 +112,26 @@ class Stepper extends StatelessWidget {
                       width: 35,
                       height: 35,
                       decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(100),
-                          color: index == currentIndex
-                              ? const Color.fromRGBO(241, 135, 137, 1)
-                              : const Color.fromRGBO(95, 95, 95, 1),
-                          border: Border.all(
-                              color: currentIndex >= index
-                                  ? const Color.fromRGBO(241, 135, 137, 1)
-                                  : Colors.transparent)),
+                        borderRadius: BorderRadius.circular(100),
+                        color: index == currentIndex
+                            ? const Color.fromRGBO(241, 135, 137, 1)
+                            : const Color.fromRGBO(95, 95, 95, 1),
+                        border: Border.all(
+                            color: currentIndex >= index
+                                ? const Color.fromRGBO(241, 135, 137, 1)
+                                : Colors.transparent),
+                      ),
+                      child: Center(
+                        child: index == currentIndex
+                            ? const Icon(Icons.edit,
+                                color: Colors
+                                    .white) // Icono de edición para el paso actual.
+                            : (currentIndex > index
+                                ? const Icon(Icons.check,
+                                    color: Colors
+                                        .white) // Icono de check para los pasos completados.
+                                : null), // Nada para los pasos que aún no se han alcanzado.
+                      ),
                     ),
                   ),
                   if (!isLast)
@@ -165,18 +153,25 @@ class Stepper extends StatelessWidget {
                 titles[index],
                 style: const TextStyle(fontWeight: FontWeight.bold),
               ),
-              ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size(15.0, 15.0),
-                    backgroundColor: index == currentIndex
-                        ? const Color.fromRGBO(241, 135, 137, 1)
-                        : const Color.fromRGBO(95, 95, 95, 1),
+              Container(
+                width: 80.0,
+                height: 25.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: index == currentIndex
+                      ? const Color.fromRGBO(241, 135, 137, 1)
+                      : const Color.fromRGBO(95, 95, 95, 1),
+                ),
+                child: Center(
+                  child: Text(
+                    currentIndex > index ? 'Completado' : 'En progreso',
+                    style: const TextStyle(color: Colors.white),
                   ),
-                  onPressed: () {},
-                  child: const Text(
-                    'En progreso',
-                    style: TextStyle(fontSize: 10, color: Colors.white),
-                  ))
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              )
             ],
           )
         : Expanded(
@@ -191,18 +186,31 @@ class Stepper extends StatelessWidget {
                       width: 35,
                       height: 35,
                       decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(100),
-                          color: index == currentIndex
-                              ? const Color.fromRGBO(241, 135, 137, 1)
-                              : const Color.fromRGBO(95, 95, 95, 1),
-                          border: Border.all(
-                              color: currentIndex >= index
-                                  ? const Color.fromRGBO(241, 135, 137, 1)
-                                  : Colors.transparent)),
+                        borderRadius: BorderRadius.circular(100),
+                        color: index == currentIndex
+                            ? const Color.fromRGBO(241, 135, 137, 1)
+                            : const Color.fromRGBO(95, 95, 95, 1),
+                        border: Border.all(
+                            color: currentIndex >= index
+                                ? const Color.fromRGBO(241, 135, 137, 1)
+                                : Colors.transparent),
+                      ),
+                      child: Center(
+                        child: index == currentIndex
+                            ? const Icon(Icons.edit,
+                                color: Colors
+                                    .white) // Icono de edición para el paso actual.
+                            : (currentIndex > index
+                                ? const Icon(Icons.check,
+                                    color: Colors
+                                        .white) // Icono de check para los pasos completados.
+                                : null), // Nada para los pasos que aún no se han alcanzado.
+                      ),
                     ),
                   ),
                   Expanded(
                       child: Container(
+                    width: 20.0,
                     height: 2,
                     color: currentIndex >= index + 1
                         ? const Color.fromRGBO(241, 135, 137, 1)
@@ -219,19 +227,144 @@ class Stepper extends StatelessWidget {
                 titles[index],
                 style: const TextStyle(fontWeight: FontWeight.bold),
               ),
-              ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size(15.0, 15.0),
-                    backgroundColor: index == currentIndex
-                        ? const Color.fromRGBO(241, 135, 137, 1)
-                        : const Color.fromRGBO(95, 95, 95, 1),
+              Container(
+                width: 80.0,
+                height: 25.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: index == currentIndex
+                      ? const Color.fromRGBO(241, 135, 137, 1)
+                      : const Color.fromRGBO(95, 95, 95, 1),
+                ),
+                child: Center(
+                  child: Text(
+                    currentIndex > index ? 'Completado' : 'En progreso',
+                    style: const TextStyle(color: Colors.white),
                   ),
-                  onPressed: () {},
-                  child: const Text(
-                    'En progreso',
-                    style: TextStyle(fontSize: 10, color: Colors.white),
-                  ))
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              )
             ],
           ));
   }
+}
+
+Widget Step1() {
+  return Column(
+    children: [
+      Container(
+        margin: const EdgeInsets.symmetric(horizontal: 20.0),
+        height: 300,
+        width: double.infinity,
+        decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 255, 255, 255),
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 4,
+                blurRadius: 4,
+                offset: const Offset(0, 3),
+              )
+            ]),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              RichText(
+                  text: const TextSpan(children: [
+                TextSpan(
+                  text: 'Nombre de la empresa ',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 15,
+                  ),
+                ),
+                TextSpan(
+                    text: '*',
+                    style: TextStyle(
+                        color: Color.fromRGBO(242, 142, 144, 1),
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold))
+              ])),
+              Form(
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.transparent,
+                    hintText: "Ingrese nombre de la empresa",
+                    hintStyle:
+                        const TextStyle(color: Color.fromRGBO(95, 95, 95, 1)),
+                    errorStyle: const TextStyle(color: Colors.redAccent),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(
+                          color: Color.fromRGBO(242, 142, 144,
+                              1)), // Color del borde en estado normal.
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(
+                          color: Color.fromRGBO(242, 142, 144,
+                              1)), // Color del borde en estado enfocado.
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value != null && RegExp(r'[0-9]').hasMatch(value)) {
+                      return 'Solo se acepta texto';
+                    }
+                    return null;
+                  },
+                  onChanged: (value) {
+                    // Tu lógica aquí
+                  },
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    ],
+  );
+}
+
+Widget Step2() {
+  return Column(
+    children: [
+      Container(
+        margin: const EdgeInsets.symmetric(horizontal: 20),
+        height: 35.0,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          color: Color.fromARGB(255, 23, 120, 89),
+        ),
+      ),
+    ],
+  );
+}
+
+Widget Step3() {
+  return Container(
+    height: 35.0,
+    width: 35.0,
+    decoration: const BoxDecoration(
+      color: Color.fromARGB(255, 37, 17, 149),
+    ),
+  );
+}
+
+Widget Step4() {
+  return Container(
+    height: 35.0,
+    width: 35.0,
+    decoration: const BoxDecoration(
+      color: Color.fromARGB(255, 175, 35, 35),
+    ),
+  );
 }
